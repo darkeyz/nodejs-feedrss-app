3e0979d3ac3bd36bf1b4ad2ab3c7865ffc62c4be
Ovrclockz <ovrclockz@gmail.com>
2018-04-25 11:40:33 +0200

    fix log seconds format

--- src/modules/log.js
+++ src/modules/log.js
@@ -43,7 +43,7 @@ const log = class Log {
 		const day = (date.getDate() < 10) ? "0" + date.getDate() : date.getDate()
 		const hours = (date.getHours() < 10) ? "0" + date.getHours() : date.getHours()
 		const minutes = (date.getMinutes() < 10) ? "0" + date.getMinutes() : date.getMinutes()
-		const secondes = (date.getSeconds() < 10) ? "0" + date.getSeconds() : date.Secondes()
+		const secondes = (date.getSeconds() < 10) ? "0" + date.getSeconds() : date.getSeconds()
 		
 		const currentDay = [date.getFullYear(), month, day].join("-")
 		const currentHour = [hours, minutes, secondes].join(":")
